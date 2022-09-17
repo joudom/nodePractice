e3d303759808166e2eaba32e8926f06f5277e488
Jesse Oudom <jesse.oudom@yahoo.com>
2022-09-16 23:26:16 -0400

    adds all small exercises

---
+++ cowsay-practice/index.js
@@ -0,0 +1,3 @@
+// S M A L L  E X E R C I S E - C o w s a y
+    const cowsay = require("cowsay");
+        console.log(cowsay.say({ text: "I'm a module" }));
---
+++ server.js
@@ -0,0 +1,19 @@
+// S M A L L  E X E R C I S E - H e l l o  N o d e
+    // console.log("Hello World, I am in Node.");
+
+// S M A L L  E X E R C I S E - H e l l o  W o r l d  S e r v e r
+    // const http = require("http");
+
+    // const hostname = "127.0.0.1";
+    // const port = 3000;
+
+    // const server = http.createServer((req, res) => {
+    //   res.statusCode = 200;
+    //   res.setHeader("Content-Type", "text/plain");
+    //   res.end("Hello World");
+    // });
+
+    // server.listen(port, hostname, () => {
+    //   console.log(`Server running at http://${hostname}:${port}/`);
+    // });
+

f205ba4860c7cabdfb7b28c1499a0f15050acecf
Jesse Oudom <jesse.oudom@yahoo.com>
2022-09-24 20:46:32 -0400

    adds 2baby-steps.js to learnyounode

---
+++ large-learnyounode/2baby-steps.js
@@ -0,0 +1,12 @@
+// MY SOLUTION
+let a = Number(process.argv[2]);
+let b = Number(process.argv[3]);
+let c = Number(process.argv[4]);
+console.log(a + b + c);
+
+// OFFICIAL SOLUTION
+// let result = 0
+// for (let i = 2; i < process.argv.length; i++) {
+//   result += Number(process.argv[i])
+// }
+// console.log(result)
